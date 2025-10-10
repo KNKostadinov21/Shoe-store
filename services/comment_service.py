a5a3fed8939823d7e85d25a38c596712481729aa
KNKostadinov21 <KNKostadinov21@codingburgas.bg>
2025-10-10 15:25:31 +0300

    Fix the functionality for adding comments.

--- services/comment_service.py
+++ services/comment_service.py
@@ -3,6 +3,6 @@ from models import db, Comment
 
 def add_comment(message):
     comment = Comment(comment=message)
-    db.session.add(message)
+    db.session.add(comment)
     db.session.commit()
     return comment
